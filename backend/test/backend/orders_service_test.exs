fa42b41521e9d1fb82cef9705752654d478cffaa
Cristian Manoel Pinheiro <cristian.m.pinheiro@gmail.com>
2022-02-02 00:46:23 -0300

    test: add test to create concurrent order creation for same user and check if balance remains consistent

--- backend/test/backend/orders_service_test.exs
+++ backend/test/backend/orders_service_test.exs
@@ -2,7 +2,7 @@ defmodule Backend.OrderServiceTest do
   use Backend.DataCase
 
   alias Backend.Orders.{Item, Order}
-  alias Backend.OrderService
+  alias Backend.{OrderService, Users}
 
   describe "order service" do
     import Backend.{ProductsFixtures, UsersFixtures}
@@ -23,6 +23,34 @@ defmodule Backend.OrderServiceTest do
 
       assert {:error, :create_order, "insufficient_balance", _} =
                OrderService.create_order(user.user_id, products_ids)
+    end
+
+    test "create_order/2 should create orders concurrently for same user and keep balance consistent" do
+      user = user_fixture(%{balance: 1000})
+
+      products = [
+        product_fixture(%{price: 150}),
+        product_fixture(%{price: 140}),
+        product_fixture(%{price: 130}),
+        product_fixture(%{price: 120}),
+        product_fixture(%{price: 110}),
+        product_fixture(%{price: 100}),
+        product_fixture(%{price: 90})
+      ]
+
+      tasks =
+        products
+        |> Enum.map(fn p ->
+          Task.async(fn ->
+            OrderService.create_order(user.user_id, [p.id])
+          end)
+        end)
+
+      Task.await_many(tasks)
+
+      {:ok, user} = Users.get_user(user.user_id)
+
+      assert 160 = user.balance
     end
   end
 end
